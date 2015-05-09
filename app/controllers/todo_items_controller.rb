f0a98ce33a663fbf7f42594c2091d98d0e92a4de
evgkit <kitaev.ea@gmail.com>
2015-05-09 22:54:44 +0300

    Add feature - Mark completed at.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -33,6 +33,12 @@ class TodoItemsController < ApplicationController
     redirect_to todo_list_todo_items_path
   end
 
+  def complete
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete."
+  end
+
   def update
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
