8504a889cf0c6f5c5911c15d44c366cc5dee185c
evgkit <kitaev.ea@gmail.com>
2015-05-10 07:48:30 +0300

    Add showing complete/incomplete items

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper'
 
 describe TodoList do
   it { should have_many(:todo_items) }
+
+  describe "#has complete items?" do
+    let(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery list.")}
+
+    it "returns true with completed todo list items" do
+      todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+      expect(todo_list.has_completed_items?).to eq(true)
+    end
+
+    it "returns false with no completed todo list items" do
+      todo_list.todo_items.create(content: "Eggs")
+      expect(todo_list.has_completed_items?).to eq(false)
+    end
+  end
+
+  describe "#has incomplete items?" do
+    let(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery list.")}
+
+    it "returns true with incompleted todo list items" do
+      todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+      expect(todo_list.has_incompleted_items?).to eq(false)
+    end
+
+    it "returns false with no incompleted todo list items" do
+      todo_list.todo_items.create(content: "Eggs")
+      expect(todo_list.has_incompleted_items?).to eq(true)
+    end
+  end
 end
