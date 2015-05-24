65f104cd7e85e45c5a8d159a4fd22198b65cd4b2
evgkit <kitaev.ea@gmail.com>
2015-05-24 19:54:42 +0300

    Add change password form

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,7 +14,14 @@ class PasswordResetsController < ApplicationController
       flash.now[:notice] = "Email not found."
       render action: 'new'
     end
+  end
 
+  def edit
+    @user = User.find_by(password_reset_token: params[:id])
+    if @user
+    else
+      render file: 'public/404.html', status: :not_found
+    end
   end
 
 end
